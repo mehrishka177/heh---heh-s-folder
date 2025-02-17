fb4a9c9e7d69f9638aad3d3b8cd3d523ed47198f
mehrishka177 <mehri@hajiagali.com>
2025-02-17 20:15:52 +0400

    lyit

---
+++ lesson_resturant_managment/lesson_god_knows_which/.b .j/kuyg.py
@@ -0,0 +1,9 @@
+# Prompt the user to input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Check if the numbers are equal
+if num1 == num2:
+    print("The two numbers are equal!")
+else:
+    print("The two numbers are not equal.")
