ce7f3435d23ba761201daadca1e56468a5994e5c
mehrishka177 <mehri@hajiagali.com>
2025-02-17 20:22:06 +0400

    uy

--- lesson_resturant_managment/lesson_god_knows_which/.b .j/kuyg.py
+++ lesson_resturant_managment/lesson_god_knows_which/.b .j/kuyg.py
@@ -2,8 +2,8 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-# Check if the numbers are equal
-if num1 == num2:
+# Check if the numbers are equal without using the comparison operator
+if (num1 ^ num2) != 0:
     print("The two numbers are equal!")
 else:
     print("The two numbers are not equal.")
