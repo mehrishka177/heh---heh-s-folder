dad7244705d0e3304b3b5cef2ae395fc916452b2
mehrishka177 <mehri@hajiagali.com>
2025-02-19 20:23:42 +0400

    ytd

---
+++ lesson_resturant_managment/lesson_god_knows_which/.b .j/kuyf/jfy.py
@@ -0,0 +1,8 @@
+# Prompt the user to input a number
+num = float(input("Enter a number: "))
+
+# Calculate the power of 2
+result = num ** 2
+
+# Print the result
+print(f"The result of {num} to the power of 2 is {result}")
