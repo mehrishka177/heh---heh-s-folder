6c0a4ff454be463e1095ad448214ac92a4ff21a2
mehrishka177 <mehri@hajiagali.com>
2025-02-19 20:25:13 +0400

    gh

--- lesson_resturant_managment/lesson_god_knows_which/.b .j/kuyf/jfy.py
+++ lesson_resturant_managment/lesson_god_knows_which/.b .j/kuyf/jfy.py
@@ -6,3 +6,8 @@ result = num ** 2
 
 # Print the result
 print(f"The result of {num} to the power of 2 is {result}")
+# Calculate the power of 4
+result_power_4 = num ** 4
+
+# Print the result
+print(f"The result of {num} to the power of 4 is {result_power_4}")
---
+++ lesson_resturant_managment/lesson_god_knows_which/.b .j/kuyf/jufty.py
@@ -0,0 +1,5 @@
+# Calculate the power of 4
+result_power_4 = num ** 4
+
+# Print the result
+print(f"The result of {num} to the power of 4 is {result_power_4}")
